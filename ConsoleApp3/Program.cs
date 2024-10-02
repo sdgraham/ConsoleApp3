1710e0cdd007ddd355dd48af223f5aff6fc91e04
sdgraham <sdgraham@SP-S-2-06-ST>
2024-10-02 10:00:32 +0100

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter your age:");
+string input = Console.ReadLine();
+
+int age = int.Parse(input);
+
+Console.WriteLine("Here's the result:");
+
+if (age < 13)
+{
+    Console.WriteLine("Thou art but a child");
+}
+else if (age < 18)
+{
+    Console.WriteLine("You are a teenage dirtbag, baby");
+}
+else if (age < 25)
+{
+    Console.WriteLine("Young adult");
+}
+else
+{
+    Console.WriteLine("Adult");
+}
+
+
+
+Console.WriteLine("program fin");
